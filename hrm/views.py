06cd7ba59d28345ec2c3f4080763359b7b4297de
Kyle Sin Lynn <kylesinlynn@gmail.com>
2023-06-10 22:32:23 +0630

    feat: create pagination for employee list

--- hrm/views.py
+++ hrm/views.py
@@ -1,5 +1,17 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+from hrm import models
+
+@login_required
 def employee_list(request):
-    return render(request, 'hrm/employee_list.html')
+    context = {}
+
+    employee_list = models.Employee.objects.all()
+    paginated_employee_list = Paginator(employee_list, 1)
+
+    page_number = request.GET.get("page")
+    context['employees'] = paginated_employee_list.get_page(page_number)
+
+    return render(request, 'hrm/employee_list.html', context)

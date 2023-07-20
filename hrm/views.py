9d4193e226263ed48ed07f387128a51e35017a89
Kyle Sin Lynn <kylesinlynn@gmail.com>
2023-07-20 20:44:28 +0630

    chore: remove unnecessary spaces

--- hrm/views.py
+++ hrm/views.py
@@ -18,10 +18,10 @@ class EmployeeListView(ListView):
                 Q(id__icontains=search_form.cleaned_data['search_query']) |
                 Q(name__icontains=search_form.cleaned_data['search_query']) |
                 Q(identity__nrc_no__icontains=search_form.cleaned_data['search_query']) |
-                Q(user__email__icontains=search_form.cleaned_data['search_query']) | 
-                Q(phone__icontains=search_form.cleaned_data['search_query']) | 
-                Q(position__name__icontains=search_form.cleaned_data['search_query']) | 
-                Q(position__team__name__icontains=search_form.cleaned_data['search_query']) | 
+                Q(user__email__icontains=search_form.cleaned_data['search_query']) |
+                Q(phone__icontains=search_form.cleaned_data['search_query']) |
+                Q(position__name__icontains=search_form.cleaned_data['search_query']) |
+                Q(position__team__name__icontains=search_form.cleaned_data['search_query']) |
                 Q(position__team__department__name__icontains=search_form.cleaned_data['search_query'])
             )
         return employee_list
@@ -35,20 +35,8 @@ class EmployeeListView(ListView):
 class EmployeeDetailView(DetailView):
     model = models.Employee
     lookup_field = 'id'
-    
+
 class EmployeeUpdateView(UpdateView):
     model = models.Employee
     lookup_field = 'id'
     form_class = forms.EmployeeForm
-
-# @login_required
-# def employee_list(request):
-#     context = {}
-
-#     employee_list = models.Employee.objects.all()
-#     paginated_employee_list = Paginator(employee_list, 100)
-
-#     page_number = request.GET.get("page")
-#     context['employees'] = paginated_employee_list.get_page(page_number)
-
-#     return render(request, 'hrm/employee_list.html', context)

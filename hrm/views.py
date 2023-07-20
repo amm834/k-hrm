8151b69f69c3dc790af2a1d840ef7fae1b06a521
Kyle Sin Lynn <kylesinlynn@gmail.com>
2023-07-20 22:09:19 +0630

    feat: create superuser only feature

--- hrm/views.py
+++ hrm/views.py
@@ -11,19 +11,22 @@ class EmployeeListView(ListView):
     paginate_by = 100
 
     def get_queryset(self):
-        search_form = self.form_class(self.request.GET)
-        employee_list = self.model.objects.filter(Q(user__is_active=True))
-        if search_form.is_valid():
-            employee_list = employee_list.filter(
-                Q(id__icontains=search_form.cleaned_data['search_query']) |
-                Q(name__icontains=search_form.cleaned_data['search_query']) |
-                Q(identity__nrc_no__icontains=search_form.cleaned_data['search_query']) |
-                Q(user__email__icontains=search_form.cleaned_data['search_query']) |
-                Q(phone__icontains=search_form.cleaned_data['search_query']) |
-                Q(position__name__icontains=search_form.cleaned_data['search_query']) |
-                Q(position__team__name__icontains=search_form.cleaned_data['search_query']) |
-                Q(position__team__department__name__icontains=search_form.cleaned_data['search_query'])
-            )
+        if self.request.user.is_superuser or self.request.user.is_staff:
+            search_form = self.form_class(self.request.GET)
+            employee_list = self.model.objects.filter(Q(user__is_active=True))
+            if search_form.is_valid():
+                employee_list = employee_list.filter(
+                    Q(id__icontains=search_form.cleaned_data['search_query']) |
+                    Q(name__icontains=search_form.cleaned_data['search_query']) |
+                    Q(identity__nrc_no__icontains=search_form.cleaned_data['search_query']) |
+                    Q(user__email__icontains=search_form.cleaned_data['search_query']) |
+                    Q(phone__icontains=search_form.cleaned_data['search_query']) |
+                    Q(position__name__icontains=search_form.cleaned_data['search_query']) |
+                    Q(position__team__name__icontains=search_form.cleaned_data['search_query']) |
+                    Q(position__team__department__name__icontains=search_form.cleaned_data['search_query'])
+                )
+        else:
+            employee_list = self.model.objects.filter(Q(user__id__exact=self.request.user.id))
         return employee_list
 
     def get_paginate_by(self):
@@ -36,7 +39,17 @@ class EmployeeDetailView(DetailView):
     model = models.Employee
     lookup_field = 'id'
 
+    def get_queryset(self):
+        if not self.request.user.is_superuser or not self.request.user.is_staff:
+            return self.model.objects.filter(Q(user__id__exact=self.request.user.id))
+        return self.model
+
 class EmployeeUpdateView(UpdateView):
     model = models.Employee
     lookup_field = 'id'
     form_class = forms.EmployeeForm
+
+    def get_queryset(self):
+        if not self.request.user.is_superuser or not self.request.user.is_staff:
+            return self.model.objects.filter(Q(user__id__exact=self.request.user.id))
+        return self.model
